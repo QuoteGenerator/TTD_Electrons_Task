e0455b48aa3a15d7c6944c2b6f1983ecacfd7863
osm208 <if25b081@technikum-wien.at>
2026-05-21 19:11:40 +0200

    Testcase 2 | Step 1

--- Electrons_around_the_core/spec/electrons_around_the_cores_spec.js
+++ Electrons_around_the_core/spec/electrons_around_the_cores_spec.js
@@ -6,4 +6,8 @@ describe('Find Electrons', function() {
 	it('should return "NO ROLLED DICES" for an empty Array', function() {
 		expect(findCores.add([])).toEqual("NO ROLLED DICES");
 	});
+
+	it('should return "NEEDS ATLEAST 4 ROLLED DICES" for [3, 6, 1]', function() {
+		expect(findCores.add([3,6,1])).toEqual("NEEDS ATLEAST 4 ROLLED DICES");
+	});
 });
