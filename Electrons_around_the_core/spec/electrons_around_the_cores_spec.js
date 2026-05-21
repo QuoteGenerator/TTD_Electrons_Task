815f42e86c595ee86dfe5ad8d6205228041aefeb
osm208 <if25b081@technikum-wien.at>
2026-05-21 20:37:30 +0200

    Testcase 6 | Step 1

--- Electrons_around_the_core/spec/electrons_around_the_cores_spec.js
+++ Electrons_around_the_core/spec/electrons_around_the_cores_spec.js
@@ -23,4 +23,7 @@ describe('Find Electrons', function() {
 		expect(findCores.add([6,6,4,4,1,3])).toEqual(2);
 	});
 
+	it('should return "12" for [ 3, 5, 3, 5, 4, 2 ]', function() {
+		expect(findCores.add([3,5,3,5,4,2])).toEqual(12);
+	});
 });
