b97727a58a3d387d91d5efd4294a5a1966957248
osm208 <if25b081@technikum-wien.at>
2026-05-21 20:27:19 +0200

    Testcase 4 | Step 3

--- Electrons_around_the_core/src/electrons_around_the_cores.js
+++ Electrons_around_the_core/src/electrons_around_the_cores.js
@@ -4,11 +4,13 @@ coresAroundElectrons.prototype.add = function(rolled_dices) {
         return "NO ROLLED DICES";
     } else if (rolled_dices.length < 4){
         return "NEEDS ATLEAST 4 ROLLED DICES";
-    } else {
+    } else if (rolled_dices.length < 6){
         let sum = 0;
-        for(let i = 0; i < rolled_dices.length; i++){
-            sum += rolled_dices[i] % 2 == 0 ? rolled_dices[i] : 0;
+        for (let i = 0; i < rolled_dices.length; ++i){
+            if(rolled_dices[i] % 2 == 0){
+                sum += rolled_dices[i];
+            }
         }
         return sum;
     }
-};
+}
