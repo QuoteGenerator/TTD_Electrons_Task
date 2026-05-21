e113a81af8c588bffb9a30f71a6668292ff03a5b
ResidentAleks123 <Aleksandartesinic720@gmail.com>
2026-05-21 21:59:14 +0200

    Testcase 6 | Step 2

--- Electrons_around_the_core/src/electrons_around_the_cores.js
+++ Electrons_around_the_core/src/electrons_around_the_cores.js
@@ -12,7 +12,41 @@ coresAroundElectrons.prototype.add = function(rolled_dices) {
             }
         }
         return sum;
-    } else {
-        return 2; //
+    } else if(rolled_dices.length == 6){
+        let multiplikator = 0;
+        let divisor = 0;
+        let sum = 0;
+
+        for(let j = 0; j < rolled_dices.length; j++){
+            if(rolled_dices[j] == rolled_dices[j+1]){
+                divisor += 1;
+                rolled_dices[j] = "d";
+                rolled_dices[j+1] = "d";
+            }
+        }
+        for(let j = 0; j < rolled_dices.length; j++){
+            for(let k = 0; k < rolled_dices.length; k++){
+                if(j != k && rolled_dices[j] == rolled_dices[k] && rolled_dices[j] != "d"){
+                    multiplikator += 1;
+                    rolled_dices[j] = "d";
+                    rolled_dices[k] = "d";
+                }
+            }
+        }
+
+        for(let i = 0; i < rolled_dices.length; i++) {
+            if(isNaN(rolled_dices[i])){continue;}
+
+            sum += rolled_dices[i];
+        }
+
+        if(multiplikator == 0){
+            multiplikator = 1;
+        }
+        if(divisor == 0){
+            divisor = 1;
+        }
+        
+        return (sum * multiplikator) / divisor;
     }
 }
