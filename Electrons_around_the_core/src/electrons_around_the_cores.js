37b70c1acf7eba4473c0a3bc277c9cfb0edee0e3
osm208 <if25b081@technikum-wien.at>
2026-05-21 19:27:22 +0200

    Testcase 2 | Step 3 (No useful refactor)

--- Electrons_around_the_core/src/electrons_around_the_cores.js
+++ Electrons_around_the_core/src/electrons_around_the_cores.js
@@ -2,7 +2,7 @@ function coresAroundElectrons() {}
 coresAroundElectrons.prototype.add = function(rolled_dices) {
     if(rolled_dices.length === 0){
         return "NO ROLLED DICES";
-    } else if(rolled_dices.length < 4){
+    } else if (rolled_dices.length < 4){
         return "NEEDS ATLEAST 4 ROLLED DICES";
     }
 };
